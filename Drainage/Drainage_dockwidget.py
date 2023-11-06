ea27164ba77734cf1151efa1104f65dcab8ab7aa
ohjuhye <juhye1056@gmail.com>
2023-11-06 11:27:47 +0900

    chore: modify Drainage_dockwidget.py

--- Drainage/Drainage_dockwidget.py
+++ Drainage/Drainage_dockwidget.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-from qgis.PyQt import uic
-from qgis.PyQt import *
-from qgis.PyQt.QtCore import Qt
-from qgis.PyQt.QtGui import QAction, QIcon
-from qgis.PyQt.QtWidgets import QMainWindow, QAction, QApplication
+from qgis.PyQt import uic, QtWidgets, QtGui
+from qgis.PyQt.QtCore import pyqtSignal
 
 # import Qtree
-from .Util import *
+from .Util import util
 from .Watershed_dialog import WatershedDialog
 from .Batch_Processor_dialog import BatchProcessor
 
@@ -50,8 +47,8 @@ class DrainageDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         # Qtree 박스에 헤더 부분 제거
         self.treeWidget.setHeaderHidden(True)
         result = _util.CheckTaudem()
-        if result == False:
-            _util.MessageboxShowError("Drainage", "Taudem is not installed.")
+        if result is False:
+            _util.messagebox_show_error("Drainage", "Taudem is not installed.")
         item10 = QtWidgets.QTreeWidgetItem(self.treeWidget, ["Drainage"])
         item16 = QtWidgets.QTreeWidgetItem(item10, ["Batch Processor"])
         icon = QtGui.QIcon(Cube)
@@ -64,17 +61,7 @@ class DrainageDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         item18 = QtWidgets.QTreeWidgetItem(item10, ["Watershed"])
         icon = QtGui.QIcon(Cube)
         item18.setIcon(0, icon)
-        # item19 = QtGui.QTreeWidgetItem(item10, ['Catchment Polygon Delination'])
-        # icon = QtGui.QIcon(Cube)
-        # item19.setIcon(0, icon)
 
-        ##좌표계정보확인용
-        # item20 = QtGui.QTreeWidgetItem(item10, ['test'])
-        # icon = QtGui.QIcon(Cube)
-        # item20.setIcon(0, icon)
-
-        #         icon = QtGui.QIcon(Drainage)
-        #         item10.setIcon(0, icon)
         self.treeWidget.expandAll()
 
         self.mainLayout = QtWidgets.QGridLayout(self)
@@ -84,35 +71,12 @@ class DrainageDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
 
     def on_double_click(self, item):
         select_item = item.text(0)
-        # 2017-12-14 박: 기존 기능 한개로 통합 (batch processor) 로 변경 기존 파일은 그대로 유지
-        # if SelectItme =='Flat':
-        #     results_dialog = FlatDialog()
-        #     results_dialog.exec_()
-        # elif SelectItme =='Fill Sink':
-        #    results_dialog = FillSinkDialog()
-        #    results_dialog.exec_()
-        # elif SelectItme =='Flow Direction':
-        #    results_dialog = Flow_DirectionDialog()
-        #    results_dialog.exec_()
-        # elif SelectItme =='Flow Accumulation':
-        #    results_dialog = Flow_AccumulationDialog()
-        #    results_dialog.exec_()
-        # elif SelectItme =='Slope':
-        #    results_dialog = SlopeDialog()
-        #    results_dialog.exec_()
-        # elif SelectItme =='Stream Definition':
-        #    results_dialog = Stream_DefinitionDialog()
-        #    results_dialog.exec_()
 
         if select_item == "Batch Processor":
             results_dialog = BatchProcessor(iface=self.iface)
             results_dialog.exec_()
-
-        # elif SelectItme =='Catchment GRID Delination':
-        #     results_dialog = CatchmentDialog()
-        #     results_dialog.exec_()
         elif select_item == "Create OutletPoint Layer and Draw OutletPoint":
-            _util.MessageboxShowInfo(
+            _util.messagebox_show_info(
                 "info",
                 "The base layer and coordinate information must be created identically.",
             )
@@ -125,7 +89,7 @@ class DrainageDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
             results_dialog = WatershedDialog()
             results_dialog.exec_()
         elif select_item == "Helps":
-            results_dialog = Watershed_StetupDialog()
+            results_dialog = Watershed_SetupDialog()
             results_dialog.exec_()
 
     def close_event(self, event):
