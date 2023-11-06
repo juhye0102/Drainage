0c027d597ff7d8cf834f954402a56168e299aaf1
ohjuhye <juhye1056@gmail.com>
2023-11-06 11:28:25 +0900

    chore: modify drainage.py

--- Drainage/drainage.py
+++ Drainage/drainage.py
@@ -17,13 +17,6 @@ class Drainage:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
         # Save reference to the QGIS interface
         self.iface = iface
         # initialize plugin directory
